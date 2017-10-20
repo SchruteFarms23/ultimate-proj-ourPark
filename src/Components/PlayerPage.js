e810a596e875d51e671bde972405c0ad2e81a558
SchruteFarms23 <romanmullady@gmail.com>
2017-10-20 14:25:12 -0400

    ok

--- src/Components/PlayerPage.js
+++ src/Components/PlayerPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 export default class PlayerPage extends React.Component{
   state = {
@@ -20,7 +21,40 @@ export default class PlayerPage extends React.Component{
   }
   render(){
     return(
+      <div>
+      <Navbar />
+      <div className="ui internally celled grid">
+  <div className="row">
+    <div className="three wide column">
+      <img/>
+    </div>
+    <div className="ten wide column">
       <h1>{this.state.player.name}</h1>
+      <p></p>
+    </div>
+    <div className="three wide column">
+      <img/>
+    </div>
+  </div>
+  <div className="row">
+    <div className="three wide column">
+      <img/>
+    </div>
+    <div className="ten wide column">
+      <p></p>
+    </div>
+    <div className="three wide column">
+      <img/>
+    </div>
+  </div>
+</div>
+</div>
     )
   }
 }
+
+
+
+
+
+// <h1>{this.state.player.name}</h1>
